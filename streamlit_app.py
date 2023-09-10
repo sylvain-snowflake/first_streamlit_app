7a1efa05ae5ef19e2bd2a37710fdeecbc1241ccd
sylvain-snowflake <144592850+sylvain-snowflake@users.noreply.github.com>
2023-09-10 12:41:07 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthly Diner')
-
-streamlit.header('Breakfast menu')
+streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
